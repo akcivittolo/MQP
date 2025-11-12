fc40409751522e75c36502003792403812d665e6
bthowes <benjaminhowes53@outlook.com>
2025-11-12 14:59:24 -0500

    Changes to be committed: 	new file:   conceptListen.py 	new file:   gui.py 	new file:   listen.py
    First changes. All of this stuff can change.

---
+++ conceptListen.py
@@ -0,0 +1,18 @@
+from pymavlink import mavutil
+
+# Use the WSL IP
+WSL_IP = "172.21.103.161"
+
+# Start a connection listening on a UDP port
+the_connection = mavutil.mavlink_connection(f'tcp:{WSL_IP}:5760')
+
+# Wait for the first heartbeat
+#   This sets the system and component ID of remote system for the link
+the_connection.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
+
+# Once connected, use 'the_connection' to get and send messages
+
+while True:
+    msg = the_connection.recv_match(blocking=True)
+    print(msg)
